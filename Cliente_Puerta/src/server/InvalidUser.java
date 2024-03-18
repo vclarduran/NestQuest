d1ce3c182719af403d28cdfce29731c3fd22f597
Ane Pradera <ane.pradera@opendeusto.es>
2024-03-18 09:24:06 +0100

    carpetas divididas

---
+++ Cliente_Puerta/src/server/InvalidUser.java
@@ -0,0 +1,24 @@
+package server;
+
+public class InvalidUser extends Exception 
+{
+
+	private String message;
+	
+	
+	public InvalidUser() {
+		super();
+		this.message = "";		
+	}
+	
+	public InvalidUser(String exceptionMessage) {
+		super();
+		this.message = exceptionMessage;
+	}
+	
+	
+	public String getErrorMessage() {
+		return this.message;
+	}
+
+}
