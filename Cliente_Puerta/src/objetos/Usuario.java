53b576703d9a37ac7bd90712eee62a41f4418af9
Violeta Clar <violeta.clar@opendeusto.es>
2024-03-18 09:45:32 +0100

    objetos

---
+++ Cliente_Puerta/src/objetos/Usuario.java
@@ -0,0 +1,14 @@
+package objetos;
+
+
+//FALTA PONER EL LOOMBOK Y GETTERS Y SETTERS
+
+public class Usuario {
+
+    private String nombre;
+    private String apellido; 
+    private String username;
+    private String contrasenya;
+    private String email; 
+    
+}
