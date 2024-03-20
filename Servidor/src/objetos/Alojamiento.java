c580b098522fb5aa0a5beb9bb2c67e5c7c478a82
Ane Pradera <ane.pradera@opendeusto.es>
2024-03-20 11:13:59 +0100

    clases alojamiento

--- Servidor/src/objetos/Alojamiento.java
+++ Servidor/src/objetos/Alojamiento.java
@@ -2,6 +2,26 @@ package objetos;
 
 public class Alojamiento {
 	
-	//prueba
+	private String nombre;
+    private String descripcion;
+    private String direccion;
+
+    public Alojamiento(String nombre, String descripcion, String direccion){
+        this.nombre=nombre;
+        this.descripcion=descripcion;
+        this.direccion=direccion;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getDescripcion(){
+        return descripcion;
+    }
+
+    public String getDireccion(){
+        return direccion;
+    }
     
 }
