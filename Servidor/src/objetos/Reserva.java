0fed1f9ce7e063fd5b17e72aac7c84cc2ebafcf8
Ane Pradera <ane.pradera@opendeusto.es>
2024-04-10 09:05:17 +0200

    reserva con JDO

--- Servidor/src/objetos/Reserva.java
+++ Servidor/src/objetos/Reserva.java
@@ -1,10 +1,15 @@
 package objetos;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
 @PersistenceCapable
 public class Reserva extends PeticionReserva{
 
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
     int idReserva; 
     Usuario personaQueReserva; 
     
