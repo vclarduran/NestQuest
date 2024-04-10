46d9cacafa1da6ec3ed914a03d8164540a0bd5a6
Ane Pradera <ane.pradera@opendeusto.es>
2024-04-10 09:17:41 +0200

    usuario JDO + atributo idUsuario

--- Servidor/src/objetos/Usuario.java
+++ Servidor/src/objetos/Usuario.java
@@ -1,10 +1,18 @@
 package objetos;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 
 //FALTA PONER EL LOOMBOK Y GETTERS Y SETTERS
 
+@PersistenceCapable
 public class Usuario {
 
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.INCREMENT)
+    private int idUsuario;
     private String nombre;
     private String apellido; 
     private String username;
