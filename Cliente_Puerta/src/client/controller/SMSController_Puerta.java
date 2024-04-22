8f876345dd0b7e937849ed5287e65a2d21892ec3
Iker Perez Gastañaga <ikperez@opendeusto.es>
2024-04-22 16:53:57 +0200

    patrones de diseño del lado cliente_puerta

---
+++ Cliente_Puerta/src/client/controller/SMSController_Puerta.java
@@ -0,0 +1,25 @@
+package client.controller;
+
+import java.rmi.RemoteException;
+
+import client.remote.RMIServiceLocator_Puerta;
+
+public class SMSController_Puerta
+{
+    private RMIServiceLocator_Puerta rsl = null;
+	
+	public SMSController_Puerta(String[] args) throws RemoteException 
+	{		
+
+        //aqui se deberian abrir las ventanas
+
+        this.rsl = new RMIServiceLocator_Puerta();
+        this.rsl.setService(args[0],args[1],args[2]);
+	}
+
+    public static void main(String[] args) throws RemoteException {    	
+    	new SMSController_Puerta(args);
+    }
+
+  
+}
---
+++ Cliente_Puerta/src/client/remote/RMIServiceLocator_Puerta.java
@@ -0,0 +1,34 @@
+package client.remote;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.IServerDoor;
+
+public class RMIServiceLocator_Puerta
+{
+    private IServerDoor service = null;
+
+    public RMIServiceLocator_Puerta(){}
+
+    public void setService(String ip, String port, String serviceName)
+    {
+        try
+        {
+            Registry registry = LocateRegistry.getRegistry(((Integer.valueOf(port))));
+			String name = "//" + ip + ":" +port + "/" + serviceName;
+			this.service = (IServerDoor) registry.lookup(name);
+			System.out.println("* Message coming from the server");
+        }
+        catch(Exception e) 
+        {
+           	System.err.println("- Exception running the client: " + e.getMessage());
+			e.printStackTrace(); 
+        }
+    }
+
+    public IServerDoor getService()
+    {
+        return service;
+    }
+}
