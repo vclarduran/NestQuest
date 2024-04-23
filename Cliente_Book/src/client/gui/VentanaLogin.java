4f1a646d7a26c129d98a0f143e731ccf570bba6f
Josu Igoa <josu.igoa@opendeusto.es>
2024-04-23 12:10:10 +0200

    Añadidas ventana ppal y login

---
+++ Cliente_Book/src/client/gui/VentanaLogin.java
@@ -0,0 +1,118 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.LayoutManager;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaLogin extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaLogin frame = new VentanaLogin();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public VentanaLogin() {
+		setTitle("Login");
+        setSize(600, 500); 
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null); 
+        
+        JPanel panelPrincipal = new JPanel(new BorderLayout());
+        JPanel panelIcono = new JPanel((LayoutManager) new FlowLayout(FlowLayout.LEFT));
+        
+        ImageIcon icono = new ImageIcon("Cliente_Book/src/client/gui/imagenes/Logo.png"); 
+        Image imagen = icono.getImage().getScaledInstance(200, 100, Image.SCALE_SMOOTH); 
+        ImageIcon iconoRedimensionado = new ImageIcon(imagen);
+        
+        JLabel labelImagen = new JLabel(iconoRedimensionado);
+        
+        panelIcono.add(labelImagen);
+        
+        panelPrincipal.add(panelIcono, BorderLayout.NORTH);
+        
+        JPanel panelLogin = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10); 
+        panelLogin.setBackground(Color.WHITE);
+        
+        JLabel labelLogin = new JLabel("Login");
+        labelLogin.setFont(new Font("Arial", Font.BOLD, 28)); 
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panelLogin.add(labelLogin, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 1;
+        panelLogin.add(new JLabel("Email:"), gbc);
+        
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        JTextField campoEmail = new JTextField(20);
+        panelLogin.add(campoEmail, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        panelLogin.add(new JLabel("Contraseña:"), gbc);
+        
+        gbc.gridx = 1;
+        JTextField campoContraseña = new JPasswordField(20);
+        panelLogin.add(campoContraseña, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JButton botonLogin = new JButton("Login");
+        panelLogin.add(botonLogin, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JLabel labelNoCuenta = new JLabel("¿No tienes cuenta?");
+        panelLogin.add(labelNoCuenta, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JButton botonCrearCuenta = new JButton("Crear cuenta");
+        botonCrearCuenta.setForeground(Color.RED);
+        panelLogin.add(botonCrearCuenta, gbc);
+               
+        panelPrincipal.add(panelLogin, BorderLayout.CENTER);
+                
+        add(panelPrincipal);
+    }
+	}
---
+++ Cliente_Book/src/client/gui/VentanaPpal.java
@@ -0,0 +1,105 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.Box;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.swing.JSplitPane;
+import javax.swing.JLayeredPane;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+public class VentanaPpal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPpal frame = new VentanaPpal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public VentanaPpal() {
+
+		 setTitle("VentanaPpal");
+	        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        
+	        JPanel panel = new JPanel(new GridBagLayout());
+	        GridBagConstraints gbc = new GridBagConstraints();
+	        gbc.fill = GridBagConstraints.HORIZONTAL;
+	        gbc.fill = GridBagConstraints.VERTICAL;
+	        gbc.insets = new Insets(10, 10, 10, 10); 
+
+	        JLabel imageLabel = new JLabel(new ImageIcon("Cliente_Book/src/client/gui/imagenes/Logo.png")); // Reemplaza con la ruta de tu imagen
+	        gbc.gridx = 0;
+	        gbc.gridy = 0;
+	        gbc.gridwidth = 1;
+	        panel.add(imageLabel, gbc);
+
+	        JLabel textLabel = new JLabel("\u00A1Reserva tu aventura!");
+	        textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+	        gbc.gridx = 0;
+	        gbc.gridy = 1;
+	        gbc.gridwidth = 1;
+	        textLabel.setFont(new Font("Arial", Font.BOLD, 20));
+	        textLabel.setForeground(Color.BLACK);
+	        panel.add(textLabel, gbc);
+
+	        JButton button = new JButton("Comenzar");
+	        button.setAlignmentX(Component.CENTER_ALIGNMENT); 
+	        button.setPreferredSize(new Dimension(150, 30)); 
+	        gbc.gridx = 0;
+	        gbc.gridy = 2;
+	        gbc.gridwidth = 1;
+	        button.setPreferredSize(new Dimension(200, 40));
+	        panel.add(button, gbc);
+	        
+	        getContentPane().add(panel, BorderLayout.CENTER);
+
+	        pack();
+	        setLocationRelativeTo(null); 
+	        setVisible(true);
+	        
+	        button.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	            
+	                VentanaLogin loginWindow = new VentanaLogin();
+	                loginWindow.setVisible(true);
+	                dispose(); 
+	            }
+	        });
+
+    }
+
+
+}
